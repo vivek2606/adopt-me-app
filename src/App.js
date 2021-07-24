385f8b9858dbf4155f91355347a2beb6acdfe575
vivek2606 <vivek.nitjsr@gmail.com>
2021-07-24 23:16:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
